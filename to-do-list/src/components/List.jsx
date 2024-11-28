5d01a7df716faaabb62645c4199d20d33e09e266
MarinaArrieta <arrietamarina12@gmail.com>
2024-11-28 16:22:56 -0300

    Cambio de title: titulo Todo List y nuevo logo

---
+++ to-do-list/src/components/List.jsx
@@ -0,0 +1,11 @@
+import Item from "./Item"
+
+const List = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default List
---
+++ to-do-list/src/components/TodoList.jsx
@@ -0,0 +1,11 @@
+import List from "./List"
+
+const TodoList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TodoList
